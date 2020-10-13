41470d96c23a07d0ed7f17ea1eaa78d5868fdb7e
sphereii <kilpatrick.jeff@gmail.com>
2020-10-13 13:30:16 -0300

    Update TileEntitySign.cs

--- Mods/0-SphereIICore/Harmony/TileEntities/TileEntitySign.cs
+++ Mods/0-SphereIICore/Harmony/TileEntities/TileEntitySign.cs
@@ -8,58 +8,6 @@ using UnityEngine;
 public class SphereII_TileEntitySign_Gif
 {
 
-    [HarmonyPatch(typeof(TileEntitySecureLootContainerSigned))]
-    [HarmonyPatch("SetText")]
-    public class SphereII_TileEntitySecureLootContainerSigned_SetText
-    {
-        public static bool Prefix(TileEntitySign __instance, SmartTextMesh[] ___smartTextMesh, string _text)
-        {
-            if (GameManager.IsDedicatedServer)
-                return true;
-
-            if (___smartTextMesh == null)
-            {
-                return true;
-            }
-            if (_text.StartsWith("http"))
-            {
-                for (int i = 0; i < ___smartTextMesh.Length; i++)
-                {
-                    Debug.Log("Component: " + ___smartTextMesh[i].transform.name);
-                    ImageWrapper wrapper = ___smartTextMesh[i].transform.parent.transform.GetComponent<ImageWrapper>();
-                    if (wrapper == null)
-                        wrapper = ___smartTextMesh[i].transform.parent.transform.gameObject.AddComponent<ImageWrapper>();
-
-                    if (wrapper.IsNewURL(_text))
-                    {
-                        wrapper.Pause();
-                        Debug.Log("Initializing ImageWrapper: " + _text + "  Transform: " + ___smartTextMesh[i].transform.name);
-                        wrapper.Init(_text, ___smartTextMesh[i].transform.name);
-
-                        __instance.SetModified();
-                    }
-                    ___smartTextMesh[i].gameObject.SetActive(false);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < ___smartTextMesh.Length; i++)
-                {
-                    ImageWrapper wrapper = ___smartTextMesh[i].transform.parent.transform.GetComponent<ImageWrapper>();
-                    if (wrapper != null)
-                    {
-
-                        //    wrapper.Reset();
-                    }
-                    ___smartTextMesh[i].gameObject.SetActive(true);
-                }
-            }
-
-            return true;
-
-        }
-    }
-
 
     [HarmonyPatch(typeof(TileEntitySign))]
     [HarmonyPatch("SetText")]
