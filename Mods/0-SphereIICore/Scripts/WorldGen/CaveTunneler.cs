3f9888dadffd70110aeb0a80b3ab8c11a7c66d36
sphereii <kilpatrick.jeff@gmail.com>
2020-10-21 19:30:08 -0300

    Update CaveTunneler.cs

--- Mods/0-SphereIICore/Scripts/WorldGen/CaveTunneler.cs
+++ Mods/0-SphereIICore/Scripts/WorldGen/CaveTunneler.cs
@@ -212,6 +212,8 @@ public static class SphereII_CaveTunneler
                 // Move from the bottom up, leaving the last few blocks untouched.
                 for (int y = tHeight; y > 5; y--)
                 {
+                    if (y > 1)
+                        break;
                     var b = chunk.GetBlock(chunkX, y, chunkZ);
 
                     if (b.type != caveAir.type)
@@ -252,9 +254,8 @@ public static class SphereII_CaveTunneler
                                 currentPrefabCount++;
 
                                 // Drop the spacing down further.
-                                y -= (prefab.size.y + 10);
+                                y -= (prefab.size.y + 5);
                                 continue;
-
                             }
                         }
                     }
