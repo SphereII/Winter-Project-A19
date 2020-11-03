140b3875adaadfe0e824e0f8b38bd4ba836246a0
sphereii <kilpatrick.jeff@gmail.com>
2020-11-03 09:53:02 -0400

    Disabling crop tramble patch

--- Mods/0-SphereIICore/Harmony/Blocks/BlockOnEntityCollidedWithBlock.cs
+++ Mods/0-SphereIICore/Harmony/Blocks/BlockOnEntityCollidedWithBlock.cs
@@ -33,48 +33,48 @@ public class SphereII_Blocks_OnEntityCollidedWithBlock
         }
     }
 
-    [HarmonyPatch(typeof(Block))]
-    [HarmonyPatch("OnEntityCollidedWithBlock")]
-    public class SphereII_Block_OnEntityCollidedWithBlock
-    {
-        public static bool Prefix(Block __instance, WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue, Entity _entity)
-        {
-            if (_entity == null)
-                return true;
+    //[HarmonyPatch(typeof(Block))]
+    //[HarmonyPatch("OnEntityCollidedWithBlock")]
+    //public class SphereII_Block_OnEntityCollidedWithBlock
+    //{
+    //    public static bool Prefix(Block __instance, WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue, Entity _entity)
+    //    {
+    //        if (_entity == null)
+    //            return false;
 
-            // Don't process if its a player.
-            if (_entity is EntityPlayerLocal)
-                return true;
+    //        // Don't process if its a player.
+    //        if (_entity is EntityPlayerLocal)
+    //            return false;
 
-            try
-            {
-                if (__instance.FilterTags != null && __instance.FilterTags.ContainsCaseInsensitive(DestructableTag))
-                    __instance.DamageBlock(_world, 0, _blockPos, _blockValue, Block.list[_blockValue.type].MaxDamage, (_entity != null) ? _entity.entityId : -1, false, false);
-            }
-            catch(Exception ex)
-            {
-                return true;
-            }
-            return true;
+    //        try
+    //        {
+    //            if (__instance.FilterTags != null && __instance.FilterTags.ContainsCaseInsensitive(DestructableTag))
+    //                __instance.DamageBlock(_world, 0, _blockPos, _blockValue, Block.list[_blockValue.type].MaxDamage, (_entity != null) ? _entity.entityId : -1, false, false);
+    //        }
+    //        catch(Exception ex)
+    //        {
+    //            return false;
+    //        }
+    //        return false;
 
 
-        }
-    }
+    //    }
+    //}
 
-    // Let the NPCs pass by traps without being hurt.
-    [HarmonyPatch(typeof(BlockDamage))]
-    [HarmonyPatch("OnEntityCollidedWithBlock")]
-    public class SphereII_BlockDamage_OnEntityCollidedWithBlock
-    {
-        public static bool Prefix(Entity _targetEntity)
-        {
-            if (_targetEntity == null)
-                return false;
+    //// Let the NPCs pass by traps without being hurt.
+    //[HarmonyPatch(typeof(BlockDamage))]
+    //[HarmonyPatch("OnEntityCollidedWithBlock")]
+    //public class SphereII_BlockDamage_OnEntityCollidedWithBlock
+    //{
+    //    public static bool Prefix(Entity _targetEntity)
+    //    {
+    //        if (_targetEntity == null)
+    //            return false;
 
-            if (_targetEntity is EntityNPC)
-                return false;
-            return true;
-        }
-    }
+    //        if (_targetEntity is EntityNPC)
+    //            return false;
+    //        return true;
+    //    }
+    //}
 
 }
